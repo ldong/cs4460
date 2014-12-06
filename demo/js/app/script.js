a44ec606f31ca8ac1eeab65052d72a3d49101977
Lin Dong <ldong@gatech.edu>
2014-12-06 14:26:31 -0500

    Update

--- demo/js/app/script.js
+++ demo/js/app/script.js
@@ -1,5 +1,6 @@
-(function(){
+requirejs(['jquery-2.1.1', 'd3', 'underscore', 'Highcharts'],
+function ($, d3, _, Highcharts) {
     console.log("Run");
-    var width = 10;
-    var height = 10;
-}());
+});
+
+
--- demo/js/config.js
+++ demo/js/config.js
@@ -1,16 +1,10 @@
 requirejs.config({
-    //By default load any module IDs from js/lib
     baseUrl: 'js/lib',
-    //except, if the module ID starts with "app",
-    //load it from the js/app directory. paths
-    //config is relative to the baseUrl, and
-    //never includes a ".js" extension since
-    //the paths config could be for a directory.
     paths: {
         'require': '../require',
         'jquery': 'jquery-2.1.1',
         'highcharts': 'highcharts',
-        'd3': './d3',
+        'd3': 'd3',
         'underscore': 'underscore',
         'coffee-script': 'coffee-script',
         app: '../app'
@@ -23,13 +17,5 @@ requirejs.config({
     }
 });
 
-// Start the main app logic.
-// requirejs(['jquery-2.1.1', 'd3.min', 'underscore'],
-// function ($, d3, _) {
-//     //jQuery, canvas and the app/sub module are all
-//     //loaded and can be used here now.
-// });
-
+// Start the main app logic
 require(['./app/script']);
-
-
